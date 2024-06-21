8349eebebf163c148c573555feb3c5965351632d
ddaazzaa <pevicsteano@gmail.com>
2024-06-21 16:47:14 +0100

    Added etch-a-sketch features and design

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const containerEl = document.querySelector(`.container`);
+const gridSizeButton = document.querySelector(`.grid-size-button`);
 
-for (i = 0; i < 256; i++) {
-  const newDivEl = document.createElement(`div`);
-  containerEl.appendChild(newDivEl);
-  newDivEl.classList.add(`box`);
+let newDivEl = ``;
+let gridSize = 16;
+
+function createGrid(size) {
+  if (gridSize <= 100) {
+    containerEl.innerHTML = ``;
+
+    for (i = 0; i < size * size; i++) {
+      newDivEl = document.createElement(`div`);
+      containerEl.appendChild(newDivEl);
+      newDivEl.classList.add(`box`);
+      const boxSize = String(960 / size);
+      newDivEl.style.width = boxSize + `px`;
+      newDivEl.style.height = boxSize + `px`;
+    }
+    const boxEl = document.querySelectorAll(`.box`);
+
+    for (i = 0; i < boxEl.length; i++) {
+      boxEl[i].addEventListener(`mouseenter`, function () {
+        this.classList.add(`hover`);
+      });
+    }
+  }
 }
 
-const boxEl = document.querySelectorAll(`.box`);
+createGrid(gridSize);
 
-for (i = 0; i < boxEl.length; i++) {
-  boxEl[i].addEventListener(`mouseenter`, function () {
-    this.classList.add(`hover`);
-  });
-}
+gridSizeButton.addEventListener(`click`, function () {
+  gridSize = Number(
+    prompt(`What dimensions do you want your grid to be? (Maximum of 100)`)
+  );
+  createGrid(gridSize);
+});
